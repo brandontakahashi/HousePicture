b36924ecd45d6ea2e1170855a6df82cec3eee240
btakahashi <brandontakahashi0@gmail.com>
2024-01-09 08:26:52 -0800

    first commit

--- src/Picture.java
+++ src/Picture.java
@@ -31,7 +31,7 @@ public class Picture
         if(!drawn) {
             wall.moveHorizontal(-140);
             wall.moveVertical(20);
-            wall.changeSize(120);
+            wall.changeSize(10);
             wall.makeVisible();
 
             window.changeColor("black");
